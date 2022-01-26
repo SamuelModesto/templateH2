17ddebf5807423c9501488059bacdb42cbef5341
Samuel Modesto <samuelsantos.aluno@unipampa.edu.br>
2022-01-26 00:12:48 -0300

    Project created

---
+++ src/main/java/com/samuel/modesto/templateH2/TemplateH2Application.java
@@ -0,0 +1,13 @@
+package com.samuel.modesto.templateH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel/modesto/templateH2/TemplateH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.modesto.templateH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
